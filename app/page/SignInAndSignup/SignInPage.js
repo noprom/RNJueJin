1db33af7ad3634848c83c62bdf450c79506a19e6
noprom <tyee.noprom@qq.com>
2016-11-27 21:24:17 +0800

    add images

--- app/page/SignInAndSignup/SignInPage.js
+++ app/page/SignInAndSignup/SignInPage.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Platform } from 'react-native';
+import { Text, View, StyleSheet, Platform, TouchableOpacity, Image, TextInput } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Button from './Button';
 
 export default class SignInPage extends Component {
+
+  _onPress() {
+
+  }
+
   render() {
     return (
       <View style={styles.view}>
-        <Text>Hello World</Text>
+        <View style={styles.actionBar}>
+          <TouchableOpacity onPress={this._onPress.bind(this)}>
+            <Icon name="md-arrow-back" size={30} color="white"/>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.logo}>
+          <Image source={require('../../image/ic_launcher.png')} />
+        </View>
       </View>
     );
   }
@@ -16,5 +30,11 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: (Platform.OS === 'ios') ? 20 : 0,
     backgroundColor: 'rgb(22, 131, 251)'
+  },
+  actionBar: {
+    margin: 15
+  },
+  logo: {
+    alignItems: 'center'
   }
 });
