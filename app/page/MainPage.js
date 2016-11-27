7b67fcb4a99f13280a5ddc7c04f469d7e83e930d
noprom <tyee.noprom@qq.com>
2016-11-27 17:30:17 +0800

    add main theme

---
+++ app/page/MainPage.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+export default class MainPage extends Component {
+  render() {
+    return (
+      <View>Main Scene</View>
+    );
+  }
+}
