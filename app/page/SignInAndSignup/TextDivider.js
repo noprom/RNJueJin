21ec711ce78c7bfb8772ad9afdde9ee7f3c44a78
noprom <tyee.noprom@qq.com>
2016-11-28 16:53:02 +0800

    add TextDivider

---
+++ app/page/SignInAndSignup/TextDivider.js
@@ -0,0 +1,43 @@
+import React, { Component, PropTypes } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Platform,
+  TouchableOpacity,
+  Image
+} from 'react-native';
+
+export default class TextDivider extends Component {
+  static propTypes = {
+    text: PropTypes.string.idRequired
+  };
+
+  render() {
+    return (
+      <View style={styles.view}>
+        <View style={styles.divider} />
+        <Text style={styles.text}>{this.props.text}</Text>
+        <View style={styles.divider} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  view: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  divider: {
+    flex: 1,
+    backgroundColor: '#3d3d3d',
+    height: 1
+  },
+  text: {
+    color: '#3d3d3d',
+    fontSize: 15,
+    marginLeft: 15,
+    marginRight: 15
+  }
+});
