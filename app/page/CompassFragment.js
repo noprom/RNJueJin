72d273b5b1b4ef83e2b94bbc9cf82f7b182746bd
noprom <tyee.noprom@qq.com>
2016-11-30 18:32:04 +0800

    add swipe

--- app/page/CompassFragment.js
+++ app/page/CompassFragment.js
@@ -4,12 +4,35 @@ import { Text, View, StyleSheet, Platform } from 'react-native';
 import px2dp from '../util/px2dp';
 import theme from '../config/theme';
 import SearchBar from '../component/SearchBar';
+import Swiper from 'react-native-swiper';
 
 export default class CompassFragment extends Component {
+
+  _searchButtonCallback() {
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <SearchBar/>
+        <SearchBar onPress={this._searchButtonCallback.bind(this)}/>
+        <Swiper
+            height={px2dp(130)}
+            autoplay={false}
+            bounces={true}>
+            <View style={styles.slide}>
+                {/*<Image style={styles.image} source={BANNER_IMAGES[0]}/>*/}
+                <View style={{backgroundColor:'red', height: px2dp(130)}}></View>
+            </View>
+            <View style={styles.slide}>
+                {/*<Image style={styles.image} source={BANNER_IMAGES[1]}/>*/}
+                <View style={{backgroundColor:'yellow', height: px2dp(130)}}></View>
+            </View>
+            <View style={styles.slide}>
+                {/*<Image style={styles.image} source={BANNER_IMAGES[2]}/>*/}
+                <View style={{backgroundColor:'green', height: px2dp(130)}}></View>
+            </View>
+        </Swiper>
       </View>
     );
   }
@@ -20,5 +43,8 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: (Platform.OS === 'ios') ? px2dp(20) : 0,
     backgroundColor: theme.pageBackgroundColor
+  },
+  slide: {
+    
   }
 });
