15ad678c89ee547413887350f77b875be07fd76a
noprom <tyee.noprom@qq.com>
2016-11-30 10:56:43 +0800

    me page ios implemented

--- app/page/MeFragment.js
+++ app/page/MeFragment.js
@@ -1,6 +1,6 @@
 'use strict';
 import React, { Component, PropTypes } from 'react';
-import { Text, View, StyleSheet, Platform, PixelRatio, TouchableNativeFeedback, ToastAndroid } from 'react-native';
+import { Text, View, StyleSheet, Platform, PixelRatio, TouchableNativeFeedback, TouchableOpacity, ToastAndroid } from 'react-native';
 import px2dp from '../util/px2dp';
 import theme from '../config/theme';
 import Avatar from '../component/Avatar';
@@ -9,7 +9,7 @@ import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class MeFragment extends Component {
   _onPressCallback(position) {
-    ToastAndroid.show('' + position, ToastAndroid.SHORT);
+    // ToastAndroid.show('' + position, ToastAndroid.SHORT);
   }
 
   render() {
@@ -18,18 +18,33 @@ export default class MeFragment extends Component {
         <View style={styles.actionBar}>
           <Text style={{color: theme.actionBar.fontColor, fontSize: theme.actionBar.fontSize}}>我</Text>
         </View>
-        <TouchableNativeFeedback onPress={this._onPressCallback.bind(this, 0)}>
-          <View style={[styles.intro]}>
-            <Avatar image={require('../image/logo_og.png')} size={px2dp(55)} textSize={px2dp(20)}/>
-            <View style={{marginLeft: px2dp(12)}}>
-              <Text style={{color: theme.text.color, fontSize: 20}}>Noprom</Text>
-              <TextButton text="添加职位 @添加公司" color="#949494" fontSize={px2dp(13)}/>
+        {Platform.OS === 'android' ?
+          <TouchableNativeFeedback onPress={this._onPressCallback.bind(this, 0)}>
+            <View style={[styles.intro]}>
+              <Avatar image={require('../image/logo_og.png')} size={px2dp(55)} textSize={px2dp(20)}/>
+              <View style={{marginLeft: px2dp(12)}}>
+                <Text style={{color: theme.text.color, fontSize: 20}}>Noprom</Text>
+                <TextButton text="添加职位 @添加公司" color="#949494" fontSize={px2dp(13)}/>
+              </View>
+              <View style={{flex: 1, flexDirection:'row', justifyContent: 'flex-end'}}>
+                <Icon name="ios-arrow-forward" color="#ccc" size={px2dp(30)}/>
+              </View>
             </View>
-            <View style={{flex: 1, flexDirection:'row', justifyContent: 'flex-end'}}>
-              <Icon name="ios-arrow-forward" color="#ccc" size={px2dp(30)}/>
+          </TouchableNativeFeedback>
+          :
+          <TouchableOpacity onPress={this._onPressCallback.bind(this, 0)}>
+            <View style={[styles.intro]}>
+              <Avatar image={require('../image/logo_og.png')} size={px2dp(55)} textSize={px2dp(20)}/>
+              <View style={{marginLeft: px2dp(12)}}>
+                <Text style={{color: theme.text.color, fontSize: 20}}>Noprom</Text>
+                <TextButton text="添加职位 @添加公司" color="#949494" fontSize={px2dp(13)}/>
+              </View>
+              <View style={{flex: 1, flexDirection:'row', justifyContent: 'flex-end'}}>
+                <Icon name="ios-arrow-forward" color="#ccc" size={px2dp(30)}/>
+              </View>
             </View>
-          </View>
-        </TouchableNativeFeedback>
+          </TouchableOpacity>
+        }
         <View style={styles.list}>
              <Item icon="md-heart" text="我的收藏" subText="15篇" iconColor="#32cd32" onPress={this._onPressCallback.bind(this, 1)}/>
              <View style={{height: 1/PixelRatio.get(), backgroundColor: '#c4c4c4'}}/>
@@ -49,35 +64,48 @@ export default class MeFragment extends Component {
   }
 }
 
-export class Item extends Component {
-  static propTypes = {
-       icon: PropTypes.string.isRequired,
-       iconColor: PropTypes.string,
-       text: PropTypes.string.isRequired,
-       subText: PropTypes.string,
-       onPress: PropTypes.func
-   }
-
-   static defaultProps = {
-       iconColor: 'gray'
-   }
-  render() {
-    const {icon, iconColor, text, subText, onPress} = this.props;
-    if (Platform.OS === 'android') {
-      return (
-         <TouchableNativeFeedback onPress={onPress}>
-         <View style={styles.listItem}>
-             <Icon name={icon} size={22} color={iconColor}/>
-             <Text style={{color: 'black', fontSize: 15, marginLeft: 20}}>{text}</Text>
-             <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end'}}>
-                 <Text style={{color: "#ccc"}}>{subText}</Text>
-             </View>
-         </View>
-       );
-    } else if (Platform.OS === 'ios') {
-
+class Item extends Component{
+    static propTypes = {
+        icon: PropTypes.string.isRequired,
+        iconColor: PropTypes.string,
+        text: PropTypes.string.isRequired,
+        subText: PropTypes.string,
+        onPress: PropTypes.func
     }
-  }
+
+    static defaultProps = {
+        iconColor: 'gray'
+    }
+
+    render(){
+        const {icon, iconColor, text, subText, onPress} = this.props;
+
+        if(Platform.OS === 'android'){
+            return(
+                <TouchableNativeFeedback onPress={onPress}>
+                    <View style={styles.listItem}>
+                        <Icon name={icon} size={px2dp(22)} color={iconColor}/>
+                        <Text style={{color: 'black', fontSize: px2dp(15), marginLeft: px2dp(20)}}>{text}</Text>
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end'}}>
+                            <Text style={{color: "#ccc"}}>{subText}</Text>
+                        </View>
+                    </View>
+                </TouchableNativeFeedback>
+            );
+        } else if(Platform.OS === 'ios'){
+            return(
+                <TouchableOpacity onPress={onPress} activeOpacity={theme.btnActiveOpacity}>
+                    <View style={styles.listItem}>
+                        <Icon name={icon} size={px2dp(22)} color={iconColor}/>
+                        <Text style={{color: 'black', fontSize: px2dp(15), marginLeft: px2dp(20)}}>{text}</Text>
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end'}}>
+                            <Text style={{color: "#ccc"}}>{subText}</Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+            );
+        }
+    }
 }
 
 const styles = StyleSheet.create({
@@ -95,25 +123,25 @@ const styles = StyleSheet.create({
   intro: {
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: 'white',
+    backgroundColor: '#fff',
     padding: px2dp(20),
     borderTopWidth: 1.5 / PixelRatio.get(),
     borderBottomWidth: 2 / PixelRatio.get(),
     borderBottomColor: '#c4c4c4',
     borderTopColor: '#e4e4e4',
   },
-   list:{
-       borderTopWidth: 1.5/PixelRatio.get(),
-       borderTopColor: '#e4e4e4',
-       marginTop: 15
+  list:{
+     borderTopWidth: 1.5 / PixelRatio.get(),
+     borderTopColor: '#e4e4e4',
+     marginTop: px2dp(15)
    },
    listItem: {
-       flex: 1,
-       height: 55,
+       flexGrow: 1,
+       height: px2dp(55),
        backgroundColor: 'white',
        flexDirection: 'row',
        alignItems: 'center',
-       paddingLeft: 25,
-       paddingRight: 25
+       paddingLeft: px2dp(25),
+       paddingRight: px2dp(25)
     }
 });
