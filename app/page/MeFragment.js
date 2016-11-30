16a7b27adc134641f1f36fe74430a4fe6a98cc61
noprom <tyee.noprom@qq.com>
2016-11-30 09:40:47 +0800

    init MeFragment

--- app/page/MeFragment.js
+++ app/page/MeFragment.js
@@ -2,12 +2,21 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet, Platform } from 'react-native';
 import px2dp from '../util/px2dp';
+import theme from '../config/theme';
+import Avatar from '../component/Avatar';
+import TextButton from '../component/TextButton';
 
 export default class MeFragment extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>MeFragment</Text>
+        <View style={[styles.intro]}>
+          <Avatar text="N"/>
+          <View>
+             <Text>noprom</Text>
+             <TextButton text="添加职位 @添加公司"/>
+          </View>
+        </View>
       </View>
     );
   }
@@ -16,6 +25,15 @@ export default class MeFragment extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: (Platform.OS === 'ios') ? px2dp(20) : 0
+    marginTop: (Platform.OS === 'ios') ? px2dp(20) : 0,
+    backgroundColor: theme.pageBackgroundColor
+  },
+  intro: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: px2dp(20),
+    borderBottomWidth: 1,
+    borderBottomColor: '#d4d4d4'
   }
 });
