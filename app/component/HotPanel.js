23eb29e5ba9beb15b21f4d1a81bdd51b41847fa8
noprom <tyee.noprom@qq.com>
2016-12-01 10:32:51 +0800

    init hot panel host list

--- app/component/HotPanel.js
+++ app/component/HotPanel.js
@@ -1,38 +1,69 @@
 'use strict';
 
-import React, {Component, PropTypes} from 'react';
-import {StyleSheet, View, Text, Dimensions, TouchableOpacity} from 'react-native';
+import React, { Component, PropTypes } from 'react';
+import { StyleSheet, View, Text, Dimensions, TouchableOpacity, PixelRatio } from 'react-native';
 import px2dp from '../util/px2dp';
 import Icon from 'react-native-vector-icons/Octicons';
 import theme from '../config/theme';
 
 export default class HotPanel extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        isHotPanelShow: true
+      }
+    }
+
     static propTypes = {
-        title: PropTypes.string.isRequired
+        title: PropTypes.string.isRequired,
+        contents: PropTypes.object
     };
 
-    render() {
-        const {title} = this.props;
+    _closeBtCallback() {
+      this.setState({isHotPanelShow: false});
+    }
 
-        return(
-            <View style={styles.container}>
-                <View style={styles.title}>
-                    <View style={styles.cell}>
-                        <Icon name="flame" color={theme.themeColor} size={15}/>
-                        <Text style={{color: theme.themeColor, fontSize: theme.scrollView.fontSize, marginLeft: 5}}>{title}</Text>
-                    </View>
-                    <View style={styles.cell}>
-                        <TouchableOpacity
-                            style={{marginRight: 15}}>
-                            <Icon name="sync" color="#c4c4c4" size={20} />
-                        </TouchableOpacity>
-                        <TouchableOpacity>
-                            <Icon name="x" color="#c4c4c4" size={20} />
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </View>
-        )
+    _refreshBtnCallback() {
+
+    }
+
+    render() {
+        const {title, contents} = this.props;
+
+        if(this.state.isHotPanelShow) {
+           return (
+               <View style={styles.container}>
+                   <View style={styles.title}>
+                       <View style={styles.cell}>
+                           <Icon name="flame" color={theme.themeColor} size={16}/>
+                           <Text style={{
+                               color: theme.themeColor,
+                               fontSize: theme.scrollView.fontSize,
+                               marginLeft: 5
+                           }}>{title}</Text>
+                       </View>
+                       <View style={styles.cell}>
+                           <TouchableOpacity
+                               style={{marginRight: 15}}
+                               onPress={this._refreshBtnCallback.bind(this)}>
+                               <Icon name="sync" color="#c4c4c4" size={20}/>
+                           </TouchableOpacity>
+                           <TouchableOpacity
+                               onPress={this._closeBtCallback.bind(this)}>
+                               <Icon name="x" color="#c4c4c4" size={20}/>
+                           </TouchableOpacity>
+                       </View>
+                   </View>
+                   <View style={{height: 1 / PixelRatio.get(), backgroundColor: '#f1f1f1'}}></View>
+                   <View style={styles.list}>
+
+                   </View>
+               </View>
+          )
+        } else {
+          return null;
+        }
     }
 }
 
@@ -51,5 +82,8 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         paddingLeft: px2dp(15),
         paddingRight: px2dp(15)
+    },
+    list: {
+      height: px2dp(30)
     }
 });
