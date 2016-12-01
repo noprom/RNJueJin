e533c7af1f2180ef3bc31caa5359ef674ee1bc87
noprom <tyee.noprom@qq.com>
2016-12-01 10:01:23 +0800

    add HotPanel

--- app/page/HomeTabPages/HomeTab.js
+++ app/page/HomeTabPages/HomeTab.js
@@ -2,21 +2,13 @@
 
 import React, { Component } from 'react';
 import { Text, StyleSheet, View } from 'react-native';
+import HotPanel from '../../component/HotPanel';
 
 export default class HomeTab extends Component {
   render() {
     return (
       <View style={{flex: 1, flexDirection:'column'}}>
-        <Text>Some</Text>
-        <Text>Some</Text>
-        <Text>Some</Text>
-        <Text>Some</Text>
-        <Text>Some</Text>
-        <Text>Some</Text>
-        <Text>Some</Text>
-        <Text>Some</Text>
-        <Text>Some</Text>
-        <Text>Some</Text>
+        <HotPanel title="热门推荐" />
       </View>
     );
   }
