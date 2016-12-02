2be0d9b6d5f8b52554e0667acdccd2437595f837
noprom <tyee.noprom@qq.com>
2016-12-02 10:47:46 +0800

    add android pkg file

--- android/app/src/main/java/com/rnjuejin/MainApplication.java
+++ android/app/src/main/java/com/rnjuejin/MainApplication.java
@@ -41,3 +41,5 @@ public class MainApplication extends Application implements ReactApplication {
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
+
+//react-native bundle --platform android --dev false --entry-file index.android.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
