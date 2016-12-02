ca52ce0a988f1f53382b33c9abc6cd557bca6a55
noprom <tyee.noprom@qq.com>
2016-12-02 08:49:55 +0800

    load data from json

--- app/page/CompassFragment.js
+++ app/page/CompassFragment.js
@@ -6,6 +6,7 @@ import theme from '../config/theme';
 import SearchBar from '../component/SearchBar';
 import Swiper from 'react-native-swiper';
 import ImageButton from '../component/ImageButtonWithText';
+import ListView from '../component/ListViewForCompass';
 
 const bannerImages = [
   require('../image/banner1.jpg'),
@@ -85,6 +86,7 @@ export default class CompassFragment extends Component {
               )
             })}
           </View>
+          <ListView />
         </ScrollView>
       </View>
     );
