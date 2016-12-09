78e2eacd71ce49e9e0418d5a02c273ba7e4300d9
noprom <tyee.noprom@qq.com>
2016-12-09 10:33:21 +0800

    init SettingPage

---
+++ app/page/SettingPage.js
@@ -0,0 +1,20 @@
+import React, { Component, PropTypes } from 'react';
+import { View, Text } from 'react-native';
+import px2dp from '../util/px2dp';
+import theme from '../config/theme';
+import NavigationBar from '../component/SimpleNavigationBar';
+
+export default class SettingPage extends Component {
+
+    _backCallback() {
+        this.props.navigator.pop();
+    }
+
+    render() {
+        return(
+            <View style={{flex: 1}}>
+                <NavigationBar title="设置" backOnPress={this._backCallback.bind(this)}/>
+            </View>
+        );
+    }
+}
